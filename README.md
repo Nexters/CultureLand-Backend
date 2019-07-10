c43db0ebeaf7405f1d9d274189307467fe5c68a8
pkch93 <star_final@naver.com>
2019-07-10 23:25:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# # CultureLand
+
+> Nexters 15th 컬쳐랜드팀 프로젝트
+
---
+++ backend/src/main/java/org/nexters/cultureland/CultureLandApplication.java
@@ -0,0 +1,13 @@
+package org.nexters.cultureland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultureLandApplication.class, args);
+    }
+
+}
