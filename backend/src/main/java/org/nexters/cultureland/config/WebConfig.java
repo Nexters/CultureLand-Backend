76f13a9a4c8e9279a222d4afe5ec875a4865eda2
pkch93 <star_final@naver.com>
2019-07-23 00:30:52 +0900

    feat: WebConfig class
    ModelMapper 빈 등록

---
+++ backend/src/main/java/org/nexters/cultureland/config/WebConfig.java
@@ -0,0 +1,14 @@
+package org.nexters.cultureland.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
