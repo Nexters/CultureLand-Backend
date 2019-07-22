32f39aa14e2a7ae799ce4d2d110975110c57a931
pkch93 <star_final@naver.com>
2019-07-23 00:30:11 +0900

    feat: DiaryDto class
    클라이언트에게서 값을 전달 받는 DiaryDto class 구현

---
+++ backend/src/main/java/org/nexters/cultureland/api/diary/DiaryDto.java
@@ -0,0 +1,11 @@
+package org.nexters.cultureland.api.diary;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DiaryDto {
+    private String title;
+    private String content;
+}
