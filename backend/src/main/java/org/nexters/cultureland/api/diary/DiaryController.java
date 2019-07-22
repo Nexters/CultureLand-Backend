5e016f169c021a63f86120e8471e8771ab15a7a6
pkch93 <star_final@naver.com>
2019-07-23 00:31:15 +0900

    feat: DiaryController class
    Diary 생성 기능 구현

--- backend/src/main/java/org/nexters/cultureland/api/diary/DiaryController.java
+++ backend/src/main/java/org/nexters/cultureland/api/diary/DiaryController.java
@@ -1,23 +1,37 @@
 package org.nexters.cultureland.api.diary;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users/{userId}/diaries")
 public class DiaryController {
 
     private final DiaryRepository diaryRepository;
+    private final ModelMapper modelMapper;
 
-    public DiaryController(final DiaryRepository diaryRepository) {
+    public DiaryController(final DiaryRepository diaryRepository, final ModelMapper modelMapper) {
         this.diaryRepository = diaryRepository;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
     public ResponseEntity<Diaries> readAllUserDiaries() {
         Diaries diaries = new Diaries(diaryRepository.findAll());
         return ResponseEntity.ok(diaries);
+    }
+
+    @PostMapping
+    public ResponseEntity createUserDiary(@PathVariable Long userId, DiaryDto diaryDto) {
+        Diary diary = diaryRepository.save(modelMapper.map(diaryDto, Diary.class));
+        String location = String.format("/users/%d/diaries/%d", userId, diary.getId());
+
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("Location", location);
+        return new ResponseEntity<>(diary, headers, HttpStatus.CREATED);
     }
 }
--- backend/src/test/java/org/nexters/cultureland/api/diary/DiaryControllerTest.java
+++ backend/src/test/java/org/nexters/cultureland/api/diary/DiaryControllerTest.java
@@ -1,5 +1,6 @@
 package org.nexters.cultureland.api.diary;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,8 +10,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -25,14 +29,19 @@ public class DiaryControllerTest {
     @MockBean
     private DiaryRepository diaryRepository;
 
-    @Test
-    void diary_전체_조회() throws Exception {
-        Diary diary = Diary.builder()
+    private Diary diary;
+
+    @BeforeEach
+    void setUp() {
+        diary = Diary.builder()
                 .id(1L)
                 .title("title")
                 .content("content")
                 .build();
+    }
 
+    @Test
+    void diary_전체_조회() throws Exception {
         when(diaryRepository.findAll()).thenReturn(Arrays.asList(diary));
 
         mockMvc.perform(get(BASE_URL)
@@ -43,5 +52,25 @@ public class DiaryControllerTest {
                 .andExpect(jsonPath("diaries.[0].id").value(1L))
                 .andExpect(jsonPath("diaries.[0].title").value("title"))
                 .andExpect(jsonPath("diaries.[0].content").value("content"));
+    }
+
+    @Test
+    void diary_생성_테스트() throws Exception {
+        String title = "title";
+        String content = "content";
+        Diary createDiary = Diary.builder()
+                .id(1L)
+                .title(title)
+                .content(content)
+                .build();
+
+        given(diaryRepository.save(any(Diary.class))).willReturn(createDiary);
+
+        mockMvc.perform(post(BASE_URL)
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .param("title", title)
+                    .param("content", content)
+                ).andDo(print())
+                .andExpect(status().isCreated());
     }
 }
