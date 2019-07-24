6fb4c3a5765e0cc733af56b25907641b3daf4a34
pkch93 <star_final@naver.com>
2019-07-24 21:10:59 +0900

    refactor: DiaryController class
    DiaryService 적용

--- backend/src/main/java/org/nexters/cultureland/api/diary/DiaryController.java
+++ backend/src/main/java/org/nexters/cultureland/api/diary/DiaryController.java
@@ -1,6 +1,5 @@
 package org.nexters.cultureland.api.diary;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +10,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users/{userId}/diaries")
 public class DiaryController {
 
-    private final DiaryRepository diaryRepository;
-    private final ModelMapper modelMapper;
+    private final DiaryService diaryService;
 
-    public DiaryController(final DiaryRepository diaryRepository, final ModelMapper modelMapper) {
-        this.diaryRepository = diaryRepository;
-        this.modelMapper = modelMapper;
+    public DiaryController(final DiaryService diaryService) {
+        this.diaryService = diaryService;
     }
 
     @GetMapping
     public ResponseEntity<Diaries> readAllUserDiaries() {
-        Diaries diaries = new Diaries(diaryRepository.findAll());
-        return ResponseEntity.ok(diaries);
+        return ResponseEntity.ok(diaryService.fetchDiaries());
     }
 
     @PostMapping
     public ResponseEntity createUserDiary(@PathVariable Long userId, DiaryDto diaryDto) {
-        Diary diary = diaryRepository.save(modelMapper.map(diaryDto, Diary.class));
-        String location = String.format("/users/%d/diaries/%d", userId, diary.getId());
+        Diary diary = diaryService.create(diaryDto);
 
+        String location = String.format("/users/%d/diaries/%d", userId, diary.getId());
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add("Location", location);
+
         return new ResponseEntity<>(diary, headers, HttpStatus.CREATED);
     }
+
+
 }
