03cdf631762ea0feebee71951d2a51c52b0b2341
pkch93 <star_final@naver.com>
2019-07-24 21:47:12 +0900

    test: DiaryController class
    기록 하나 조회 및 존재하지 않는 데이터 조회시 테스트

--- backend/src/test/java/org/nexters/cultureland/api/diary/DiaryControllerTest.java
+++ backend/src/test/java/org/nexters/cultureland/api/diary/DiaryControllerTest.java
@@ -2,6 +2,7 @@ package org.nexters.cultureland.api.diary;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.nexters.cultureland.api.diary.excepion.NotFoundDiaryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,7 +28,7 @@ public class DiaryControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private DiaryRepository diaryRepository;
+    private DiaryService diaryService;
 
     private Diary diary;
 
@@ -42,11 +43,11 @@ public class DiaryControllerTest {
 
     @Test
     void diary_전체_조회() throws Exception {
-        when(diaryRepository.findAll()).thenReturn(Arrays.asList(diary));
+        when(diaryService.fetchDiaries()).thenReturn(new Diaries(Arrays.asList(diary)));
 
         mockMvc.perform(get(BASE_URL)
-                    .contentType(MediaType.APPLICATION_JSON_UTF8)
-                ).andDo(print())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+        ).andDo(print())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("diaries.[0].id").value(1L))
@@ -64,13 +65,40 @@ public class DiaryControllerTest {
                 .content(content)
                 .build();
 
-        given(diaryRepository.save(any(Diary.class))).willReturn(createDiary);
+        given(diaryService.create(any(DiaryDto.class))).willReturn(createDiary);
 
         mockMvc.perform(post(BASE_URL)
-                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                    .param("title", title)
-                    .param("content", content)
-                ).andDo(print())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("title", title)
+                .param("content", content)
+        ).andDo(print())
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void diary_id_1번_데이터_조회() throws Exception {
+        when(diaryService.getDiaryOf(1L)).thenReturn(diary);
+
+        mockMvc.perform(get(BASE_URL + "/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+        ).andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.title").value("title"))
+                .andExpect(jsonPath("$.content").value("content"));
+    }
+
+    @Test
+    void 존재하지_않는_diary_조회_에러() throws Exception {
+        String errorMessage = "존재하지 않는 기록입니다.";
+        when(diaryService.getDiaryOf(2L)).thenThrow(new NotFoundDiaryException(errorMessage));
+
+        mockMvc.perform(get(BASE_URL + "/2")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+        ).andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(errorMessage));
     }
 }
