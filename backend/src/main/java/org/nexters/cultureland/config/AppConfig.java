15d8abdf8c510d58461b2b24b41452d4244b90e5
pkch93 <star_final@naver.com>
2019-07-24 21:09:43 +0900

    refactor: AppConfig class
    WebConfig -> AppConfig

--- backend/src/main/java/org/nexters/cultureland/config/AppConfig.java
+++ backend/src/main/java/org/nexters/cultureland/config/AppConfig.java
@@ -3,10 +3,9 @@ package org.nexters.cultureland.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
