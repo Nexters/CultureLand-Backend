cf817275097a68f7f900b1c2ccba9ee2d2a70dfe
pkch93 <star_final@naver.com>
2019-07-28 14:12:50 +0900

    feat: Diary class
    기록 작성시 필요한 컬럼 추가 / Dto 반영

--- backend/src/main/java/org/nexters/cultureland/api/diary/DiaryDto.java
+++ backend/src/main/java/org/nexters/cultureland/api/diary/DiaryDto.java
@@ -9,10 +9,17 @@ import lombok.Setter;
 @NoArgsConstructor
 public class DiaryDto {
     private String title;
+    private String sometime;
+    private String place;
+    private String withWho;
     private String content;
 
-    public DiaryDto(final String title, final String content) {
+    public DiaryDto(final String title, final String sometime,
+                    final String place, final String withWho, final String content) {
         this.title = title;
+        this.sometime = sometime;
+        this.place = place;
+        this.withWho = withWho;
         this.content = content;
     }
 }
