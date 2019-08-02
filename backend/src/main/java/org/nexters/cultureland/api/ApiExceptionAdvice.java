ee2d1a67dbec8a66b41a0247de2fbfd829d8674e
Taehyun <ddd2520@naver.com>
2019-08-02 17:35:34 +0900

    Add ArgumentResolver (HttpServletRequest -> long)

--- backend/src/main/java/org/nexters/cultureland/api/ApiExceptionAdvice.java
+++ backend/src/main/java/org/nexters/cultureland/api/ApiExceptionAdvice.java
@@ -1,5 +1,6 @@
 package org.nexters.cultureland.api;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.nexters.cultureland.common.ResponseMessage;
 import org.nexters.cultureland.common.excepion.NotFoundResouceException;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ApiExceptionAdvice {
@@ -18,5 +21,14 @@ public class ApiExceptionAdvice {
         ResponseMessage responseMessage = new ResponseMessage(exception.getMessage(), 400, null,null);
 
         return responseMessage;
+    }
+
+    @ExceptionHandler({ExpiredJwtException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseMessage ForbiddenException(HttpServletRequest request, ExpiredJwtException e){
+        //String error, int code, String message, String path
+        System.out.println(e.getMessage());
+        return new ResponseMessage(e.getMessage(), HttpStatus.FORBIDDEN.value(),
+                null, request.getServletPath());
     }
 }
--- backend/src/main/java/org/nexters/cultureland/config/WebConfig.java
+++ backend/src/main/java/org/nexters/cultureland/config/WebConfig.java
@@ -3,21 +3,29 @@ package org.nexters.cultureland.config;
 import org.nexters.cultureland.common.JwtServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-/*        registry.addInterceptor(tokenInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/signInOrUp", "/error");*/
-
-
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArugmentResolver());
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/signInOrUp", "/error");
+    }
+    @Bean
+    public UserArugmentResolver userArugmentResolver(){
+        return new UserArugmentResolver();
+    }
     @Bean
     public TokenInterceptor tokenInterceptor(){
         return new TokenInterceptor(new JwtServiceImpl());
