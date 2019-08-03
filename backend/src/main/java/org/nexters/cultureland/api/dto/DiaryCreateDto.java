cf3908468b2c66c60ffc30875685bfefc18041d8
pkch93 <star_final@naver.com>
2019-08-03 16:29:39 +0900

    feat: 상황에 따른 DiaryDto 구현
    - Diary 생성시 사용하는 DiaryCreateDto 구현
    - Diary 수정시 사용하는 DiaryUpdateDto 구현

---
+++ backend/src/main/java/org/nexters/cultureland/api/dto/DiaryCreateDto.java
@@ -0,0 +1,29 @@
+package org.nexters.cultureland.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DiaryCreateDto {
+    private String title;
+    private String sometime;
+    private String place;
+    private String withWho;
+    private String content;
+    private String cultureName;
+
+    @Builder
+    private DiaryCreateDto(final String title, final String sometime, final String place,
+                          final String withWho, final String content, final String cultureName) {
+        this.title = title;
+        this.sometime = sometime;
+        this.place = place;
+        this.withWho = withWho;
+        this.content = content;
+        this.cultureName = cultureName;
+    }
+}
