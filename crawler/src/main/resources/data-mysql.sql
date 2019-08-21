a0ab0b530c1950a2ec3fffce06fd6b79997b7764
ezslvr <jieun08@naver.com>
2019-08-21 09:20:59 +0900

    Database etc 추가

--- crawler/src/main/resources/data-mysql.sql
+++ crawler/src/main/resources/data-mysql.sql
@@ -2,3 +2,4 @@ insert into culture (id, culture_name) values (null, 'musical');
 insert into culture (id, culture_name) values (null, 'concert');
 insert into culture (id, culture_name) values (null, 'play');
 insert into culture (id, culture_name) values (null, 'exhibition');
+insert into culture (id, culture_name) values (null, 'etc');
